cf4b003a17b51211c7521f2aaca20902e0e15590
Ji-Hun-Jeong <147147285+Ji-Hun-Jeong@users.noreply.github.com>
2023-12-26 00:43:37 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -5,51 +5,44 @@
 #include <set>
 #include <algorithm>
 using namespace std;
+const int INF = 1000001;
+int dp[1000001];
+int result[1000001];
 int N;
-int arr[500];
-int rSize = 500;
-int result[500];
-void Copy(int* const dest, const int* const src, int n)
-{
-	for (int i = 0; i <= n; ++i)
-		dest[i] = src[i];
-}
-void dfs(int idx)
-{
-	if (idx >= rSize)
-		return;
-	int num = arr[idx];
-	if (num == 1)
-	{
-		if (idx < rSize)
-		{
-			Copy(result, arr, idx);
-			rSize = idx;
-		}
-		return;
-	}
-	if (0 == num % 3)
-	{
-		arr[idx+1] = num / 3;
-		dfs(idx + 1);
-	}
-	if (0 == num % 2)
-	{
-		arr[idx+1] = num / 2;
-		dfs(idx + 1);
-	}
-	arr[idx+1] = num - 1;
-	dfs(idx + 1);
-}
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 	cin >> N;
-	arr[0] = N;
-	dfs(0);
-	cout << rSize << "\n";
-	for (int i = 0; i <= rSize; ++i)
-		cout << result[i] << " ";
+	for (int i = 1; i <= N; i++)
+	{
+		dp[i] = INF;
+	}
+	dp[1] = 0;
+	result[1] = -1;
+	for (int i = 2; i <= N; ++i)
+	{
+		dp[i] = dp[i - 1] + 1;
+		result[i] = i - 1;
+		if (i % 3 == 0 && dp[i / 3] + 1 < dp[i])
+		{
+			dp[i] = dp[i / 3] + 1;
+			result[i] = i / 3;
+		}
+		if (i % 2 == 0 && dp[i / 2] + 1 < dp[i])
+		{
+			dp[i] = dp[i / 2] + 1;
+			result[i] = i / 2;
+		}
+	}
+	int idx = N;
+	cout << dp[N] << "\n";
+	cout << N << " ";
+	while (result[idx] != -1)
+	{
+		cout << result[idx] << " ";
+		idx = result[idx];
+	}
 }
