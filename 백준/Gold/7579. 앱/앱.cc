4dccafa43015060faa1e87a354c494999e796c75
Ji-Hun-Jeong <147147285+Ji-Hun-Jeong@users.noreply.github.com>
2024-01-20 23:07:25 +0900

    [Gold III] Title: 앱, Time: 4 ms, Memory: 5968 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.cc
+++ 백준/Gold/7579. 앱/앱.cc
@@ -7,7 +7,7 @@
 using namespace std;
 int N, M;
 pair<int, int> arr[101];
-int dp[10000001][2];
+int dp[10001][101];
 int main()
 {
 	ios::sync_with_stdio(false);
@@ -15,33 +15,30 @@ int main()
 	cout.tie(nullptr);
 	cin >> N >> M;
 	int sum = 0;
-	int result = 0;
 	for (int i = 1; i <= N; ++i)
-	{
 		cin >> arr[i].first;
-		sum += arr[i].first;
-	}
 	for (int i = 1; i <= N; ++i)
 	{
 		cin >> arr[i].second;
-		result += arr[i].second;
+		sum += arr[i].second;
 	}
-	int order = 1;
-	int m = sum - M;
 	for (int j = 1; j <= N; ++j)
 	{
-		for (int i = 1; i <= m; ++i)
+		for (int i = 0; i <= sum; ++i)
 		{
-			int idx = i - arr[j].first;
+			int idx = i - arr[j].second;
 			if (idx >= 0)
-				dp[i][order] = max(dp[idx][!order] + arr[j].second, dp[i][!order]);
+				dp[i][j] = max(dp[idx][j - 1] + arr[j].first, dp[i][j - 1]);
 			else
-				dp[i][order] = dp[i][!order];
+				dp[i][j] = dp[i][j - 1];
 		}
-		order = !order;
 	}
-	int Max = 0;
-	for (int i = 0; i <= m; ++i)
-		Max = max(Max, dp[i][!order]);
-	cout << result - Max;
+	for (int i = 0; i <= sum; ++i)
+	{
+		if (dp[i][N] >= M)
+		{
+			cout << i;
+			break;
+		}
+	}
 }
