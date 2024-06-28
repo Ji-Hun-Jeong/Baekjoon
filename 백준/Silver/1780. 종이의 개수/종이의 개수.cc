c8371bc1cd616d396829344d2f0501b52c5d3661
Ji-Hun-Jeong <147147285+Ji-Hun-Jeong@users.noreply.github.com>
2024-06-28 14:43:40 +0900

    [Silver II] Title: 종이의 개수, Time: 1084 ms, Memory: 20704 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int N = 0;
+int arr[2187][2187] = { 0 };
+int a = 0, b = 0, c = 0;
+bool Check(int sx, int sy, int n)
+{
+	for (int i = sy; i < sy + n; ++i)
+		for (int j = sx; j < sx + n; ++j)
+			if (arr[sy][sx] != arr[i][j])
+				return false;
+	return true;
+}
+void f(int sx, int sy, int n)
+{
+	bool check = Check(sx, sy, n);
+	if (check)
+	{
+		arr[sy][sx] == -1 ? ++a : (arr[sy][sx] == 0 ? ++b : ++c);
+		return;
+	}
+	f(sx, sy, n / 3);
+	f(sx + n / 3, sy, n / 3);
+	f(sx + n * 2 / 3, sy, n / 3);
+	f(sx, sy + n / 3, n / 3);
+	f(sx + n / 3, sy + n / 3, n / 3);
+	f(sx + n * 2 / 3, sy + n / 3, n / 3);
+	f(sx, sy + n * 2 / 3, n / 3);
+	f(sx + n / 3, sy + n * 2 / 3, n / 3);
+	f(sx + n * 2 / 3, sy + n * 2 / 3, n / 3);
+}
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+		for (int j = 0; j < N; ++j)
+			cin >> arr[i][j];
+	f(0, 0, N);
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+}
