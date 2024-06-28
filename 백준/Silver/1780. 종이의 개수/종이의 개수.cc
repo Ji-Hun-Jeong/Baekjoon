f361a6ae7f6a7c7e00c9bc8d5661191f5d14bfd9
Ji-Hun-Jeong <147147285+Ji-Hun-Jeong@users.noreply.github.com>
2024-06-28 14:47:04 +0900

    [Silver II] Title: 종이의 개수, Time: 368 ms, Memory: 20704 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -32,6 +32,9 @@ void f(int sx, int sy, int n)
 }
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	cin >> N;
 	for (int i = 0; i < N; ++i)
 		for (int j = 0; j < N; ++j)
