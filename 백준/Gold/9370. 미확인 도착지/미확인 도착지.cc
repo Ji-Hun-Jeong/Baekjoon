e4590020ceb647b85fd10cadbf0783768666f86d
Ji-Hun-Jeong <147147285+Ji-Hun-Jeong@users.noreply.github.com>
2026-01-26 10:03:37 +0900

    [Gold II] Title: 미확인 도착지, Time: 36 ms, Memory: 3336 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
@@ -1,117 +1,90 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 #include <string>
-#include <set>
 #include <algorithm>
-using namespace std;
-#define INF 987654321
-int n, m, t, s, g, h;
-struct Edge
+#include <climits>
+
+enum { MaxVertex = 2001 };
+int32_t n, m, t;
+int32_t s, g, h;
+
+struct TEdge
 {
-	int destNum;
-	int distance;
+	int32_t Vertex;
+	int32_t Weight;
 };
-bool operator >(const Edge& first, const Edge& second)
+
+bool operator < (const TEdge& InA, const TEdge& InB)
 {
-	return first.distance > second.distance;
-}
-int sg, sh, gh;
-void Init(int* dist,bool* visited)
-{
-	for (int i = 1; i <= n; ++i)
-	{
-		visited[i]= false;
-		dist[i] = INF;
-	}
-}
-int Djikstra(const vector<Edge>* node,int* dist,bool* visited,int start, int end)
-{
-	Init(dist, visited);
-	priority_queue<Edge, vector<Edge>, greater<Edge>> q;
-	dist[start] = 0;
-	q.push({ start,0 });
-	while (!q.empty())
-	{
-		Edge edge = q.top(); q.pop();
-		int curNodeNum = edge.destNum;
-		if (visited[curNodeNum])
-			continue;
-		visited[curNodeNum] = true;
-		for (const Edge& e : node[curNodeNum])
-		{
-			int adjNodeNum = e.destNum;
-			if (dist[curNodeNum]+ e.distance < dist[adjNodeNum])
-			{
-				dist[adjNodeNum] = dist[curNodeNum] + e.distance;
-				q.push({ adjNodeNum,dist[adjNodeNum] });
-			}
-		}
-	}
-	return dist[end];
-}
-void Calculate()
-{
-	
+	return InA.Weight > InB.Weight;
 }
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-	int testK = 0;
-	cin >> testK;
-	while (testK--)
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	int32_t T;
+	std::cin >> T;
+	while (T--)
 	{
-		vector<Edge> node[2001];
-		int firstDjikstraResult[2001];
-		int dist[2001];
-		bool visited[2001];
-		vector<int> vecDestNum;
-		vector<int> finalDests;
-		cin >> n >> m >> t;
-		cin >> s >> g >> h;
-		for (int i = 0; i < m; ++i)
-		{
-			int a, b, d;
-			cin >> a >> b >> d;
-			if ((a == g && b == h) || (a == h) && (b == g))
-				gh = d;
-			node[a].push_back({ b,d });
-			node[b].push_back({ a,d });
-		}
-		for (int i = 0; i < t; ++i)
-		{
-			int destNum = 0;
-			cin >> destNum;
-			vecDestNum.push_back(destNum);
-		}
-		Djikstra(node,dist,visited,s, n);
-		sg = dist[g];
-		sh = dist[h];
-		for (int dest : vecDestNum)
-			firstDjikstraResult[dest] = dist[dest];
-		for (int dest : vecDestNum)
-		{
-			int dist1, dist2;
+		std::cin >> n >> m >> t;
+		std::cin >> s >> g >> h;
 
-			dist1 = sg + gh + Djikstra(node, dist, visited, h, dest);
-			if (dist1 == firstDjikstraResult[dest])
-			{
-				finalDests.push_back(dest);
-				continue;
-			}
+		std::vector<TEdge> VertexEdges[MaxVertex];
+		for (int32_t i = 0; i < m; ++i)
+		{
+			int32_t a, b, d;
+			std::cin >> a >> b >> d;
+			if ((a == g && b == h) || (a == h && b == g))
+				d = 2 * d - 1;
+			else
+				d = 2 * d;
+			VertexEdges[a].emplace_back(b, d);
+			VertexEdges[b].emplace_back(a, d);
+		}
+		int32_t MinDist[MaxVertex];
+		std::fill(MinDist + 1, MinDist + n + 1, INT_MAX);
+		MinDist[s] = 0;
+		
+		std::priority_queue<TEdge> Edges;
+		Edges.emplace(s, 0);
+		while (Edges.empty() == false)
+		{
+			TEdge Edge = Edges.top();
+			Edges.pop();
 
-			dist2 = sh + gh + Djikstra(node, dist, visited, g, dest);
-			if (dist2 == firstDjikstraResult[dest])
-			{
-				finalDests.push_back(dest);
+			int32_t Vertex = Edge.Vertex;
+			int32_t Weight = Edge.Weight;
+
+			if (MinDist[Vertex] < Weight)
 				continue;
+
+			const auto& AdjEdges = VertexEdges[Vertex];
+			for (const auto& AdjEdge : AdjEdges)
+			{
+				int32_t NextVertex = AdjEdge.Vertex;
+				int32_t NextWeight = AdjEdge.Weight;
+
+				int32_t FinalWeight = MinDist[Vertex] + NextWeight;
+				if (FinalWeight < MinDist[NextVertex])
+				{
+					MinDist[NextVertex] = FinalWeight;
+					Edges.emplace(NextVertex, FinalWeight);
+				}
 			}
 		}
-		sort(finalDests.begin(), finalDests.end());
-		for (const int& i : finalDests)
-			cout << i << " ";
-		cout << "\n";
+
+		int32_t Dest[101];
+		for (int32_t i = 1; i <= t; ++i)
+			std::cin >> Dest[i];
+		std::sort(Dest + 1, Dest + t + 1);
+		for (int32_t i = 1; i <= t; ++i)
+		{
+			int32_t Index = Dest[i];
+			if (MinDist[Index] != INT_MAX && MinDist[Index] % 2 == 1)
+				std::cout << Index << " ";
+		}
+		std::cout << '\n';
 	}
 }
