ba09eeefce436c4c016dafadd962e6dab29198d9
Ji-Hun-Jeong <147147285+Ji-Hun-Jeong@users.noreply.github.com>
2026-01-31 10:36:07 +0900

    [Silver II] Title: 종이의 개수, Time: 304 ms, Memory: 20704 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,46 +1,80 @@
 #include <iostream>
+#include <queue>
 #include <string>
-using namespace std;
-int N = 0;
-int arr[2187][2187] = { 0 };
-int a = 0, b = 0, c = 0;
-bool Check(int sx, int sy, int n)
-{
-	for (int i = sy; i < sy + n; ++i)
-		for (int j = sx; j < sx + n; ++j)
-			if (arr[sy][sx] != arr[i][j])
-				return false;
-	return true;
+#include <algorithm>
+#include <climits>
+#include <array>
+
+constexpr int32_t Pow(int32_t InX, int32_t InP) {
+	return (InP == 0) ? 1 : InX * Pow(InX, InP - 1);
 }
-void f(int sx, int sy, int n)
+constexpr int32_t MaxValue = Pow(3, 7); 
+enum {Max = MaxValue};
+
+int32_t N;
+int32_t Paper[Max][Max];
+enum class EState { Zero, One, NOne, Devide };
+
+int32_t Nums[3];
+void Devide(int32_t InX, int32_t InY, int32_t InSize)
 {
-	bool check = Check(sx, sy, n);
-	if (check)
+	int32_t Sum = 0;
+	bool bDirty = false;
+	for (int32_t i = InY; i < InY + InSize; ++i)
+		for (int32_t j = InX; j < InX + InSize; ++j)
+		{
+			Sum += Paper[i][j];
+			if (Paper[i][j] == 1 || Paper[i][j] == -1)
+				bDirty = true;
+		}
+
+	EState State;
+	if (Sum == 0 && bDirty == false)
+		State = EState::Zero;
+	else if (Sum == InSize * InSize)
+		State = EState::One;
+	else if (-Sum == InSize * InSize)
+		State = EState::NOne;
+	else
+		State = EState::Devide;
+
+	if (State == EState::Devide)
 	{
-		arr[sy][sx] == -1 ? ++a : (arr[sy][sx] == 0 ? ++b : ++c);
-		return;
+		int32_t DevideSize = InSize / 3;
+		int32_t X1 = InX + DevideSize;
+		int32_t X2 = X1 + DevideSize;
+		int32_t Y1 = InY + DevideSize;
+		int32_t Y2 = Y1 + DevideSize;
+		Devide(InX, InY, DevideSize);
+		Devide(X1, InY, DevideSize);
+		Devide(X2, InY, DevideSize);
+		Devide(InX, Y1, DevideSize);
+		Devide(X1, Y1, DevideSize);
+		Devide(X2, Y1, DevideSize);
+		Devide(InX, Y2, DevideSize);
+		Devide(X1, Y2, DevideSize);
+		Devide(X2, Y2, DevideSize);
 	}
-	f(sx, sy, n / 3);
-	f(sx + n / 3, sy, n / 3);
-	f(sx + n * 2 / 3, sy, n / 3);
-	f(sx, sy + n / 3, n / 3);
-	f(sx + n / 3, sy + n / 3, n / 3);
-	f(sx + n * 2 / 3, sy + n / 3, n / 3);
-	f(sx, sy + n * 2 / 3, n / 3);
-	f(sx + n / 3, sy + n * 2 / 3, n / 3);
-	f(sx + n * 2 / 3, sy + n * 2 / 3, n / 3);
+	else
+		Nums[static_cast<size_t>(State)] += 1;
+
 }
+
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> N;
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < N; ++j)
-			cin >> arr[i][j];
-	f(0, 0, N);
-	cout << a << endl;
-	cout << b << endl;
-	cout << c << endl;
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	std::cin >> N;
+
+	for (int32_t i = 0; i < N; ++i)
+		for (int32_t j = 0; j < N; ++j)
+			std::cin >> Paper[i][j];
+
+	Devide(0, 0, N);
+
+	std::cout << Nums[static_cast<size_t>(EState::NOne)] << '\n';
+	std::cout << Nums[static_cast<size_t>(EState::Zero)] << '\n';
+	std::cout << Nums[static_cast<size_t>(EState::One)];
 }
