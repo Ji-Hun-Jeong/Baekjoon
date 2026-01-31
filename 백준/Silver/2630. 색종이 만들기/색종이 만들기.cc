6782a822267c009a8fb9e2e401c19ac4a77d1f33
Ji-Hun-Jeong <147147285+Ji-Hun-Jeong@users.noreply.github.com>
2026-01-31 09:31:10 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,64 +1,70 @@
 #include <iostream>
-#include <vector>
+#include <queue>
+#include <string>
 #include <algorithm>
-using namespace std;
+#include <climits>
+#include <array>
 
-void cal(vector<int>* pVec
-	, int startX, int finishX
-	, int startY, int finishY
-	, int& blue, int& white)
+enum { Max = 128 };
+int32_t N;
+bool Paper[Max][Max];
+int32_t NumWhite = 0;
+int32_t NumBlue = 0;
+
+enum class EPaperType
 {
-	bool check = true;
-	int color = pVec[startY][startX];
-	for (int i = startY; i < finishY; ++i)
-	{
-		for (int j = startX; j < finishX; ++j)
-		{
-			if (color != pVec[i][j])
-			{
-				check = false;
-				break;
-			}
-		}
-	}
-	if (check)
-	{
-		if (1 == color)
-			++blue;
-		else
-			++white;
-		return;
-	}
+	White,
+	Blue,
+	Devide
+};
+void Devide(int32_t InStartX, int32_t InEndX, int32_t InStartY, int32_t InEndY)
+{
+	int32_t Sum = 0;
+	for (int32_t i = InStartY; i <= InEndY; ++i)
+		for (int32_t j = InStartX; j <= InEndX; ++j)
+			Sum += Paper[i][j];
+	
+	EPaperType PaperType;
+	if (Sum == 0)
+		PaperType = EPaperType::White;
+	else if (Sum == (InEndX + 1 - InStartX) * (InEndY + 1 - InStartY))
+		PaperType = EPaperType::Blue;
 	else
+		PaperType = EPaperType::Devide;
+
+	switch (PaperType)
 	{
-		int diffX = finishX - startX;
-		int diffY = finishY - startY;
-		cal(pVec, startX, startX + diffX / 2, startY, startY + diffY / 2, blue, white);
-		cal(pVec, startX, startX + diffX / 2, startY + diffY / 2, finishY, blue, white);
-		cal(pVec, startX + diffX / 2, finishX, startY, startY + diffY / 2, blue, white);
-		cal(pVec, startX + diffX / 2, finishX, startY + diffY / 2, finishY, blue, white);
+	case EPaperType::White:
+		NumWhite += 1;
+		break;
+	case EPaperType::Blue:
+		NumBlue += 1;
+		break;
+	case EPaperType::Devide:
+	{
+		int32_t HalfSizeX = (InEndX + InStartX) / 2;
+		int32_t HalfSizeY = (InEndY + InStartY) / 2;
+		Devide(InStartX, HalfSizeX, InStartY, HalfSizeY);
+		Devide(InStartX, HalfSizeX, HalfSizeY + 1, InEndY);
+		Devide(HalfSizeX + 1, InEndX, InStartY, HalfSizeY);
+		Devide(HalfSizeX + 1, InEndX, HalfSizeY + 1, InEndY);
+	}
+		break;
+	default:
+		break;
 	}
 }
 int main()
 {
-	int n;
-	cin >> n;
-	vector<int>* pVec = new vector<int>[n];
-	for (int i = 0; i < n; ++i)
-	{
-		pVec[i].reserve(n);
-	}
-	int num;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < n; ++j)
-		{
-			cin >> num;
-			pVec[i].push_back(num);
-		}
-	}
-	int blue = 0;
-	int white = 0;
-	cal(pVec, 0, n, 0, n, blue, white);
-	cout << white << endl << blue;
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	std::cin >> N;
+	for (int32_t i = 0; i < N; ++i)
+		for (int32_t j = 0; j < N; ++j)
+			std::cin >> Paper[i][j];
+
+	Devide(0, N - 1, 0, N - 1);
+	std::cout << NumWhite << '\n' << NumBlue;
 }
