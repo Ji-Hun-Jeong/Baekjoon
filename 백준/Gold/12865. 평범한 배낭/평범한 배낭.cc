bd49692c869fd861319c47f517fd8629258093dd
Ji-Hun-Jeong <147147285+Ji-Hun-Jeong@users.noreply.github.com>
2026-02-04 11:05:20 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,27 +1,34 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int N, K;
-int DP[2][100001];
-int W[101];
-int V[101];
-
+int32_t DP[100001];
 
 int main()
 {
-	cin >> N >> K;
-	for (int i = 1; i <= N; i++)
-		cin >> W[i] >> V[i];
-	int order = 1;
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= K; j++)
-		{
-			if (j - W[i] >= 0) DP[order][j] = max(DP[!order][j], DP[!order][j - W[i]] + V[i]);
-			else DP[order][j] = DP[!order][j];
-		}
-		order = !order;
-	}
-	cout << DP[!order][K];
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int32_t N, K;
+    cin >> N >> K;
+
+    for (int32_t i = 0; i < N; ++i)
+    {
+        int32_t W, V;
+        cin >> W >> V;
+
+        for (int32_t j = K; j >= W; --j)
+        {
+            if (DP[j] < DP[j - W] + V)
+            {
+                DP[j] = DP[j - W] + V;
+            }
+        }
+    }
+
+    cout << DP[K];
+
+    return 0;
 }
