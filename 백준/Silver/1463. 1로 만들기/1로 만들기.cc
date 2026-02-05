933f2dbc1414d66aa04e406b3efe89e4cd8bbccc
Ji-Hun-Jeong <147147285+Ji-Hun-Jeong@users.noreply.github.com>
2026-02-05 09:46:14 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,27 +1,33 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 #include <string>
-#include <set>
 #include <algorithm>
-using namespace std;
-int dp[1000001];
+#include <climits>
+#include <array>
+
+enum {Max = 1000001};
+uint16_t DP[Max];
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-	int n;
-	cin >> n;
-	std::fill(dp, dp + 1000001, 1000001);
-	dp[1] = 0;
-	for (int i = 1; i <= n; ++i)
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+
+	int32_t N;
+	std::cin >> N;
+	std::fill(DP, DP + Max, USHRT_MAX);
+	DP[1] = 0;
+	for (int32_t i = 1; i <= N; ++i)
 	{
-		if (i * 3 <= n)
-			dp[i * 3] = min(dp[i * 3], dp[i] + 1);
-		if (i * 2 <= n)
-			dp[i * 2] = min(dp[i * 2], dp[i] + 1);
-		dp[i + 1] = min(dp[i + 1], dp[i] + 1);
+		int32_t _3 = i * 3;
+		int32_t _2 = i * 2;
+		int32_t _1 = i + 1;
+		if (_3 <= N)
+			DP[_3] = std::min<uint16_t>(DP[i] + 1, DP[_3]);
+		if (_2 <= N)
+			DP[_2] = std::min<uint16_t>(DP[i] + 1, DP[_2]);
+		if (_1 <= N)
+			DP[_1] = std::min<uint16_t>(DP[i] + 1, DP[_1]);
 	}
-	cout << dp[n];
+	std::cout << DP[N];
 }
