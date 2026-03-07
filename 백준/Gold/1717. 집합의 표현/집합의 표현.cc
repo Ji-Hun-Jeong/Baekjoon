57b3ef99adad784b94fff9cd5ffb65b251e98631
Ji-Hun-Jeong <147147285+Ji-Hun-Jeong@users.noreply.github.com>
2026-03-07 10:24:24 +0900

    [Gold V] Title: 집합의 표현, Time: 1508 ms, Memory: 5988 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <print>
+#include <array>
+#include <vector>
+
+std::array<int32_t, 1000001> Parents;
+
+int32_t GetRoot(size_t InValue)
+{
+	if (InValue == Parents[InValue])
+		return InValue;
+	return GetRoot(Parents[InValue]);
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	
+	int32_t N, M;
+	std::cin >> N >> M;
+
+	for (int32_t i = 1; i <= N; ++i)
+	{
+		Parents[i] = i;
+	}
+
+
+	for (int32_t i = 0; i < M; ++i)
+	{
+		int32_t A, B, C;
+		std::cin >> A >> B >> C;
+
+		int32_t RootB = GetRoot(B);
+		int32_t RootC = GetRoot(C);
+		if (A == 0)
+		{
+			if (RootB == RootC)
+				continue;
+
+			Parents[RootC] = RootB;
+		}
+		else
+		{
+			if (RootB == RootC)
+				std::cout << "YES\n";
+			else
+				std::cout << "NO\n";
+		}
+	}
+}
